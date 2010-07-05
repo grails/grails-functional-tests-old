6043ad7edd8bea817698912172c37754a5bc06c6
Graeme Rocher <graeme.rocher@gmail.com>
2010-07-05 15:30:24 +0200

    functional test for GRAILS-6420

---
+++ apps/maven-plugins/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,30 @@
+grails.project.work.dir = "target/work"
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+		
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+	plugins {
+		compile ':hibernate:1.3.3.BUILD-SNAPSHOT'
+	}
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    }
+}
