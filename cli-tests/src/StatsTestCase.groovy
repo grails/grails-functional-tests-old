c0f67b924763be926c5b00c9dda9fb26b07d9d28
Jeff Brown <jeff@jeffandbetsy.net>
2010-07-12 10:02:00 -0500

    GRAILS-6489 - added StatsTestCase

---
+++ cli-tests/src/StatsTestCase.groovy
@@ -0,0 +1,33 @@
+class StatsTestCase extends AbstractCliTestCase {
+	def appSource = new File(cliTestsDir, "testing-app")
+	def app = new File(baseWorkDir, "stats-testing-app")
+	
+	StatsTestCase() {
+		copyDir(appSource, app)
+		workDir = app
+		execute(["upgrade", "-non-interactive"])
+		assertEquals("upgrade failed", 0, waitForProcess())
+	}
+	
+	void testStats() {
+		execute([ "stats" ])
+		assertEquals 0, waitForProcess()
+		assertTrue output.contains('| Domain Classes       |     1 |     3 |')
+		assertTrue output.contains('| Services             |     1 |     3 |')
+		assertTrue output.contains('| Unit Tests           |     1 |     6 |')
+		assertTrue output.contains('| Integration Tests    |     3 |    69 |')
+		assertTrue output.contains('| Totals               |     6 |    81 |')
+	}
+
+	def copyDir(source, destination) {
+		if (source.directory) {
+			if (!destination.exists()) {
+				assert destination.mkdir() : "making $destination"
+			}
+			source.eachFile { copyDir(it, new File(destination, it.name)) }
+		} else {
+			destination.createNewFile()
+			destination << source.newInputStream()
+		}
+	}
+}
