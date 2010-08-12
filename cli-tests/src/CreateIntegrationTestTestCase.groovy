94fb188f0e64d6d0a0573e3d516441a5d9720c04
Jeff Brown <jeff@jeffandbetsy.net>
2010-08-12 12:01:21 -0500

    GRAILS-6381 - test suffix stripping for test creation

--- cli-tests/src/CreateIntegrationTestTestCase.groovy
+++ cli-tests/src/CreateIntegrationTestTestCase.groovy
@@ -14,4 +14,16 @@ class CreateIntegrationTestTestCase extends AbstractCliTestCase {
         
         assertTrue 'integration test case was not created', new File(appDir, 'test/integration/com/demo/integration/HelloWorldTests.groovy').exists()
     }
+    
+    void testSuffixStripping() {
+        execute([ 'create-integration-test' ])
+        enterInput 'com.demo.integration.DemoTests'
+        
+        assertEquals 0, waitForProcess()
+        
+        def appDir = new File(baseWorkDir, 'app1')
+        
+        assertTrue 'integration test case was not created', new File(appDir, 'test/integration/com/demo/integration/DemoTests.groovy').exists()
+        assertFalse 'test with wrong name was created', new File(appDir, 'test/integration/com/demo/integration/DemoTestsTests.groovy').exists()
+    }
 }
--- cli-tests/src/CreateUnitTestTestCase.groovy
+++ cli-tests/src/CreateUnitTestTestCase.groovy
@@ -14,4 +14,16 @@ class CreateUnitTestTestCase extends AbstractCliTestCase {
         
         assertTrue 'unit test case was not created', new File(appDir, 'test/unit/com/demo/unit/HelloWorldTests.groovy').exists()
     }
+    
+    void testSuffixStripping() {
+        execute([ "create-unit-test" ])
+        enterInput "com.demo.unit.DemoTests"
+        
+        assertEquals 0, waitForProcess()
+        
+        def appDir = new File(baseWorkDir, 'app1')
+        
+        assertTrue 'unit test case was not created', new File(appDir, 'test/unit/com/demo/unit/DemoTests.groovy').exists()
+        assertFalse 'test with wrong name was created', new File(appDir, 'test/unit/com/demo/unit/DemoTestsTests.groovy').exists()
+    }
 }
