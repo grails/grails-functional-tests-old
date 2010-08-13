e5efa229e345c8d69ae4bf81be21081fd10456b9
Jeff Brown <jeff@jeffandbetsy.net>
2010-08-13 11:58:50 -0500

    GRAILS-6604 - functional test to support GRAILS-6604 currently marked with notYetImplemented() as a solution is developed.  This problem started with http://github.com/grails/grails-core/commit/3a0e57a1cd847517796d8d8deaa1524364d11849.

---
+++ apps/null-binding/test/functional/BindingToNullableDateFunctionalTests.groovy
@@ -0,0 +1,19 @@
+class BindingToNullableDateFunctionalTests extends functionaltestplugin.FunctionalTestCase {
+    void testJeff() {
+        // http://jira.codehaus.org/browse/GRAILS-6604
+        if(notYetImplemented()) return
+        get('/widget/create')
+        assertStatus 200
+        assertContentContains '<title>Create Widget</title>'
+
+		form {
+		      manufactureDate_day = ""
+		      manufactureDate_month = ""
+		      manufactureDate_year = ""
+		      click "create"
+		}
+		
+        assertStatus 200
+        assertContentContains '<title>Show Widget</title>'
+    }
+}
