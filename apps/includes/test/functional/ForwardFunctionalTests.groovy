e486dab250c7a0615538460563561ccacdc72691
Jeff Brown <jeff@jeffandbetsy.net>
2010-08-25 14:23:12 -0500

    GRAILS-6618 - remove notYetImplemented() from relevant test.

--- apps/includes/test/functional/ForwardFunctionalTests.groovy
+++ apps/includes/test/functional/ForwardFunctionalTests.groovy
@@ -14,8 +14,6 @@ class ForwardFunctionalTests extends functionaltestplugin.FunctionalTestCase {
 
 	 void testForwardToOtherController() {
 	     // GRAILS-6618
-	     if(notYetImplemented()) return
-	     
 		  get('/forward/forwardToOther?dummy=test')
 		  assertStatus 200
 		  assertContentContains 'OtherController page.gsp'
