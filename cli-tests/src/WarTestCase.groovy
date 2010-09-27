ead1e916baf253ee5eb7c435863acb4ee2b68668
Graeme Rocher <graeme.rocher@gmail.com>
2010-09-27 16:50:42 +0200

    functional test for GRAILS-6708 "Building a war with the Hibernate plugin set via BuildConfig.groovy does not ship the Hibernate jars"

--- cli-tests/src/WarTestCase.groovy
+++ cli-tests/src/WarTestCase.groovy
@@ -29,6 +29,8 @@ class WarTestCase extends AbstractCliTestCase {
 
         assertTrue "aspectjweaver-1.6.8.jar file is missing from the WAR",
                    warEntries.contains("WEB-INF/lib/aspectjweaver-1.6.8.jar")
+        assertTrue "hibernate-core-3.3.1.jar file is missing from the WAR",
+                   warEntries.contains("WEB-INF/lib/hibernate-core-3.3.1.GA.jar")
         assertTrue "log4j-1.2.15.jar file is missing from the WAR",
                    warEntries.contains("WEB-INF/lib/log4j-1.2.16.jar")
         assertTrue "grails-bootstrap-${grailsVersion}.jar file is missing from the WAR",
