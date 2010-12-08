6488f3da3e86c1cc21474964227c69d973a7ceb4
Jeff Brown <jeff@jeffandbetsy.net>
2010-12-08 16:35:30 -0600

    GRAILS-6221 - test that a url may be mapped directly to a view even if multiple view resolvers are present

--- apps/view-resolution/grails-app/conf/BuildConfig.groovy
+++ apps/view-resolution/grails-app/conf/BuildConfig.groovy
@@ -1,2 +1,10 @@
 grails.project.work.dir = "target/work"
 grails.plugin.location.'plugin-views' = 'plugins/plugin-views-0.1'
+grails.project.dependency.resolution = {
+    inherits('global') {
+    }
+    repositories {
+        grailsPlugins()
+        grailsCentral()
+    }
+}
