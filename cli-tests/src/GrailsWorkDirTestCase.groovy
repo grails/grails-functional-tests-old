5c4006f58bec1cd80223700e6a8658b8f90f2018
Jeff Brown <jeff@jeffandbetsy.net>
2010-12-15 17:24:16 -0600

    GRAILS-6232 - add cli test to cover the working directory name when creating a new app

--- cli-tests/src/GrailsWorkDirTestCase.groovy
+++ cli-tests/src/GrailsWorkDirTestCase.groovy
@@ -17,18 +17,38 @@ class GrailsWorkDirTestCase extends AbstractCliTestCase {
         // move the project to a directory name that does not match the application name
         assertTrue 'renaming project directory failed', new File(baseWorkDir, applicationName).renameTo(new File(baseWorkDir,dirName))
 
-        workDir = new File(baseWorkDir, dirName)
+        def originalWorkDir = workDir
+        try {
+            workDir = new File(baseWorkDir, dirName)
 
-        def commandsToRun = ['compile', 'war', 'test-app']
-        
-        commandsToRun.each { cmd ->
-            execute([ cmd ])
+            def commandsToRun = ['compile', 'war', 'test-app']
 
-            assertEquals 0, waitForProcess()
+            commandsToRun.each { cmd ->
+                execute([ cmd ])
 
-            assertTrue "app work dir should exist after ${cmd}", new File(grailsWorkDirPath, "projects/$applicationName").exists()
-            assertFalse "bad work dir should not exist after ${cmd}", new File(grailsWorkDirPath, "projects/$dirName").exists()
+                assertEquals 0, waitForProcess()
+
+                assertTrue "app work dir should exist after ${cmd}", new File(grailsWorkDirPath, "projects/$applicationName").exists()
+                assertFalse "bad work dir should not exist after ${cmd}", new File(grailsWorkDirPath, "projects/$dirName").exists()
+            }
+        } finally {
+            workDir = originalWorkDir
         }
+    }
+
+    void testWorkDirIsNotBasedOnCurrentDirectoryNameWhenCreatingNewApp() {
+        def grailsWorkDirPath = System.getProperty('grails.work.dir')
+        new File(grailsWorkDirPath, "projects/.core").delete()
+
+        assertFalse new File(grailsWorkDirPath, "projects/${workDir.name}").exists()
+        assertFalse new File(grailsWorkDirPath, "projects/.core").exists()
+
+        execute([ "create-app" ])
+        enterInput 'anothernewapp'
+
+        assertEquals 0, waitForProcess()
+        assertFalse new File(grailsWorkDirPath, "projects/${workDir.name}").exists()
+        assertTrue new File(grailsWorkDirPath, "projects/.core").exists()
     }
 }
 
