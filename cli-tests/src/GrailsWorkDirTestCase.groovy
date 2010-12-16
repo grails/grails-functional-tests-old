695445831e2555b300be02bb92deb3e35d2ff16e
Jeff Brown <jeff@jeffandbetsy.net>
2010-12-15 21:11:50 -0600

    GRAILS-6232 - fix tests

--- cli-tests/src/GrailsWorkDirTestCase.groovy
+++ cli-tests/src/GrailsWorkDirTestCase.groovy
@@ -4,19 +4,18 @@ class GrailsWorkDirTestCase extends AbstractCliTestCase {
         def applicationName = 'someappname'
         def dirName = 'somedirname'
         def grailsWorkDirPath = System.getProperty('grails.work.dir')
-        assertFalse 'app work dir should not exist before creating the app', new File(grailsWorkDirPath, "projects/$applicationName").exists()
-        assertFalse 'bad work dir should not exist before creating the app', new File(grailsWorkDirPath, "projects/$dirName").exists()
+        assertFalse 'app work dir should not exist before creating the app', new File(baseWorkDir, "$applicationName").exists()
+        assertFalse 'bad work dir should not exist before creating the app', new File(baseWorkDir, "$dirName").exists()
 
         execute([ "create-app" ])
         enterInput applicationName
 
         assertEquals 0, waitForProcess()
-        assertTrue 'app work dir should exist after creating the app', new File(grailsWorkDirPath, "projects/$applicationName").exists()
-        assertFalse 'bad work dir should not exist after creating the app', new File(grailsWorkDirPath, "projects/$dirName").exists()
+        assertTrue 'app work dir should exist after creating the app', new File(baseWorkDir, "$applicationName").exists()
+        assertFalse 'bad work dir should not exist after creating the app', new File(baseWorkDir, "$dirName").exists()
 
         // move the project to a directory name that does not match the application name
         assertTrue 'renaming project directory failed', new File(baseWorkDir, applicationName).renameTo(new File(baseWorkDir,dirName))
-
         def originalWorkDir = workDir
         try {
             workDir = new File(baseWorkDir, dirName)
@@ -40,8 +39,8 @@ class GrailsWorkDirTestCase extends AbstractCliTestCase {
         def grailsWorkDirPath = System.getProperty('grails.work.dir')
         new File(grailsWorkDirPath, "projects/.core").delete()
 
-        assertFalse new File(grailsWorkDirPath, "projects/${workDir.name}").exists()
-        assertFalse new File(grailsWorkDirPath, "projects/.core").exists()
+        assertFalse new File(grailsWorkDirPath, "${workDir.name}").exists()
+        assertFalse new File(grailsWorkDirPath, ".core").exists()
 
         execute([ "create-app" ])
         enterInput 'anothernewapp'
