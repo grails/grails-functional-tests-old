4d71a3dd053fedebe9a550e76e84326f75fe8bdd
Jeff Brown <jeff@jeffandbetsy.net>
2011-01-20 20:13:39 -0600

    GRAILS-4204 - comment out test related to GRAILS-4204 (which is still open). I will put a comment on that JIRA so this test is not lost or forgotten

--- apps/view-resolution/test/functional/ViewResolutionFunctionalTests.groovy
+++ apps/view-resolution/test/functional/ViewResolutionFunctionalTests.groovy
@@ -36,7 +36,7 @@ class ViewResolutionFunctionalTests extends functionaltestplugin.FunctionalTestC
         assertStatus 200
         assertContentContains 'plugin default = bar'		
 	}
-
+/*
     void testNestedRelativeView() {
         // TODO: nested relative views not supported yet. See GRAILS-4204
         if(notYetImplemented()) return
@@ -44,6 +44,7 @@ class ViewResolutionFunctionalTests extends functionaltestplugin.FunctionalTestC
         assertStatus 200
         assertContentContains 'simple = bar'
     }
+    */
 	void testNestedRelativeTemplate() {
         get('/pluginTest/testNestedRelativeTemplate')
         assertStatus 200
