0f1d7e23e645fbdbfcbbe95fd3841ce49c3605f2
Jeff Brown <jeff@jeffandbetsy.net>
2011-01-21 13:37:16 -0600

    merge the headers app into the kitchen_sink_app

--- apps/kitchen_sink_app/test/functional/headers/CompressHeadersFunctionalTests.groovy
+++ apps/kitchen_sink_app/test/functional/headers/CompressHeadersFunctionalTests.groovy
@@ -1,3 +1,5 @@
+package headers
+
 class CompressHeadersFunctionalTests extends functionaltestplugin.FunctionalTestCase {
     void testCompressedOutput() {
         // test for GRAILS-4183: If this headers are broken then garbbled output will result
