31607db24278a4d2ea39f7cfb69e410bb38efdab
Jeff Brown <jeff@jeffandbetsy.net>
2011-01-26 21:44:50 -0600

    GRAILS-7189 - Allow nullable date properties to be updated to be null after they have a valid value

--- apps/kitchen_sink_app/test/functional/nullbinding/BindingToNullableDateFunctionalTests.groovy
+++ apps/kitchen_sink_app/test/functional/nullbinding/BindingToNullableDateFunctionalTests.groovy
@@ -7,14 +7,66 @@ class BindingToNullableDateFunctionalTests extends functionaltestplugin.Function
         assertStatus 200
         assertContentContains '<title>Create Widget</title>'
 
-		form {
-		      manufactureDate_day = ""
-		      manufactureDate_month = ""
-		      manufactureDate_year = ""
-		      click "create"
-		}
-		
+        form {
+            manufactureDate_day = ""
+            manufactureDate_month = ""
+            manufactureDate_year = ""
+            click "create"
+        }
+
         assertStatus 200
         assertContentContains '<title>Show Widget</title>'
+    }
+
+    void testBindingNullToNullableDatePropertyThatAlreadyHasANonNullValue() {
+        // http://jira.codehaus.org/browse/GRAILS-7189
+        get '/widget/create'
+        assertStatus 200
+        assertContentContains '<title>Create Widget</title>'
+
+        form {
+            manufactureDate_day = "15"
+            manufactureDate_month = "11"
+            manufactureDate_year = "1969"
+            click "create"
+        }
+
+        assertStatus 200
+        assertContentContains '<title>Show Widget</title>'
+        assertContentContains '1969-11-15'
+        form {
+            click '_action_edit'
+        }
+        assertStatus 200
+        assertContentContains 'Edit Widget'
+
+        form {
+            manufactureDate_day = ""
+            manufactureDate_month = ""
+            manufactureDate_year = ""
+            click "_action_update"
+        }
+
+        assertStatus 200
+        assertContentContains '<title>Show Widget</title>'
+        assertContentDoesNotContain '1969-11-15'
+
+        form {
+            click '_action_edit'
+        }
+
+        assertStatus 200
+        assertContentContains 'Edit Widget'
+
+        form {
+            manufactureDate_day = "16"
+            manufactureDate_month = "11"
+            manufactureDate_year = "1969"
+            click "_action_update"
+        }
+
+        assertStatus 200
+        assertContentContains '<title>Show Widget</title>'
+        assertContentContains '1969-11-16'
     }
 }
