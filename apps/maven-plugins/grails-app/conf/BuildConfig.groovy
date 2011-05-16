3b3f122156253867617563c8b8b5b855d496726f
Jeff Brown <jeff@jeffandbetsy.net>
2011-05-16 18:54:32 -0500

    add hsqldb as a runtime dependency on most apps.  kitchen_sink_app is using h2

--- apps/maven-plugins/grails-app/conf/BuildConfig.groovy
+++ apps/maven-plugins/grails-app/conf/BuildConfig.groovy
@@ -29,5 +29,6 @@ grails.project.dependency.resolution = {
 	}
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        runtime 'hsqldb:hsqldb:1.8.0.10'
     }
 }
--- apps/modify-beans/grails-app/conf/BuildConfig.groovy
+++ apps/modify-beans/grails-app/conf/BuildConfig.groovy
@@ -30,5 +30,6 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
+        runtime 'hsqldb:hsqldb:1.8.0.10'
     }
 }
