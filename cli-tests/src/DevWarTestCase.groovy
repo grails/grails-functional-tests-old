a1120a34c571e7777899846dbb3283e81c46344e
Graeme Rocher <graeme.rocher@gmail.com>
2011-05-18 15:09:06 +0200

    fix for failing functional CLI tests

--- cli-tests/src/DevWarTestCase.groovy
+++ cli-tests/src/DevWarTestCase.groovy
@@ -46,9 +46,11 @@ class DevWarTestCase extends AbstractCliTestCase {
             }
         }
 
-        assertTrue "aspectjweaver-1.6.8.jar file is missing from the WAR",
-                   warEntries.contains("WEB-INF/lib/aspectjweaver-1.6.8.jar")
-        assertTrue "log4j-1.2.15.jar file is missing from the WAR",
+        assertTrue "aspectjweaver-1.6.10.jar file is missing from the WAR",
+                   warEntries.contains("WEB-INF/lib/aspectjweaver-1.6.10.jar")
+        assertTrue "hibernate-core-3.6.1.Final.jar file is missing from the WAR",
+                   warEntries.contains("WEB-INF/lib/hibernate-core-3.6.1.Final.jar")
+        assertTrue "log4j-1.2.16.jar file is missing from the WAR",
                    warEntries.contains("WEB-INF/lib/log4j-1.2.16.jar")
         assertTrue "grails-bootstrap-${grailsVersion}.jar file is missing from the WAR",
                    warEntries.contains("WEB-INF/lib/grails-bootstrap-${grailsVersion}.jar".toString())
