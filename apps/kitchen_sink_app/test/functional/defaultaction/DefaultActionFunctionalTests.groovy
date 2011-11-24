8baf4fb4cc0af00c98dc4a368077190a93d23079
Jeff Brown <jeff@jeffandbetsy.net>
2011-11-24 12:40:13 -0600

    GRAILS-8367 - Test default action in scaffolded controllers

--- apps/kitchen_sink_app/test/functional/defaultaction/DefaultActionFunctionalTests.groovy
+++ apps/kitchen_sink_app/test/functional/defaultaction/DefaultActionFunctionalTests.groovy
@@ -20,4 +20,53 @@ class DefaultActionFunctionalTests extends functionaltestplugin.FunctionalTestCa
         assertStatus 200
         assertContentContains 'the upload action closure rendered this'
 	}
+    
+    void testDefaultActionOnScaffoldedControllerWithOneMethodAction() {
+        get '/scaffoldedWithOneExplicitMethodAction'
+        assertStatus 200
+        assertContentContains 'Child List'
+        
+        get '/scaffoldedWithOneExplicitMethodAction/test'
+        assertStatus 200
+        assertContentContains 'Rendered by the ScaffoldedWithOneExplicitMethodActionController.test method action'
+        
+    }
+    
+    void testDefaultActionOnScaffoldedControllerWithMultipleMethodActions() {
+        get '/scaffoldedWithMultipleExplicitMethodActions'
+        assertStatus 200
+        assertContentContains 'Child List'
+        
+        get '/scaffoldedWithMultipleExplicitMethodActions/one'
+        assertStatus 200
+        assertContentContains 'Rendered by the ScaffoldedWithMultipleExplicitMethodActionsController.one method action'
+        
+        get '/scaffoldedWithMultipleExplicitMethodActions/two'
+        assertStatus 200
+        assertContentContains 'Rendered by the ScaffoldedWithMultipleExplicitMethodActionsController.two method action'
+    }
+
+    void testDefaultActionOnScaffoldedControllerWithOneClosureAction() {
+        get '/scaffoldedWithOneExplicitClosureAction'
+        assertStatus 200
+        assertContentContains 'Child List'
+        
+        get '/scaffoldedWithOneExplicitClosureAction/test'
+        assertStatus 200
+        assertContentContains 'Rendered by the ScaffoldedWithOneExplicitClosureActionController.test closure action'
+    }
+    
+    void testDefaultActionOnScaffoldedControllerWithMultipleClosureActions() {
+        get '/scaffoldedWithMultipleExplicitClosureActions'
+        assertStatus 200
+        assertContentContains 'Child List'
+        
+        get '/scaffoldedWithMultipleExplicitClosureActions/one'
+        assertStatus 200
+        assertContentContains 'Rendered by the ScaffoldedWithMultipleExplicitClosureActionsController.one closure action'
+        
+        get '/scaffoldedWithMultipleExplicitClosureActions/two'
+        assertStatus 200
+        assertContentContains 'Rendered by the ScaffoldedWithMultipleExplicitClosureActionsController.two closure action'
+    }
 }
