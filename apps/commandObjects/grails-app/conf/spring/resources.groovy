71e856523a1d2abdecf5880d6744178173c63f4b
Jeff Brown <jeff@jeffandbetsy.net>
2012-04-13 13:05:54 -0500

    Revert "test command object validation"
    This reverts commit c19b6ec34a2c40fca3a9946aee458fe72cd47dcc.

--- apps/commandObjects/grails-app/conf/spring/resources.groovy
+++ apps/commandObjects/grails-app/conf/spring/resources.groovy
@@ -1,6 +1,4 @@
 // Place your Spring DSL code here
 beans = {
-    myValidationHelper(com.demo.MyValidationHelper) {
-        minimumValidValue = 42
-    }
+    
 }
--- apps/commandObjects/src/groovy/com/demo/MyValidationHelper.groovy
+++
@@ -1,5 +0,0 @@
-package com.demo
-
-class MyValidationHelper {
-    def minimumValidValue
-}
