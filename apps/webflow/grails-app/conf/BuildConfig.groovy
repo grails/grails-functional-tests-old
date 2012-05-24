1e8f16d4ef4f1138f01d3ec43fedf760197c0e44
Jeff Brown <jeff@jeffandbetsy.net>
2012-05-24 09:46:56 -0500

    use :webflow:1.3.8

--- apps/webflow/grails-app/conf/BuildConfig.groovy
+++ apps/webflow/grails-app/conf/BuildConfig.groovy
@@ -21,7 +21,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     plugins {
-		runtime ":webflow:${grailsVersion}"
+		runtime ":webflow:1.3.8"
 	}
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
