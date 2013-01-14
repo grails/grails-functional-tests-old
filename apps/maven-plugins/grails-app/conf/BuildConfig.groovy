6976d65308200435e8985272766f0b9b683c942a
Graeme Rocher <graeme.rocher@gmail.com>
2013-01-14 14:12:10 +0100

    fix maven-plugins project compilation errors

--- apps/maven-plugins/grails-app/conf/BuildConfig.groovy
+++ apps/maven-plugins/grails-app/conf/BuildConfig.groovy
@@ -3,6 +3,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -17,19 +18,22 @@ grails.project.dependency.resolution = {
         //mavenLocal()
         //mavenCentral()
         mavenRepo "http://snapshots.repository.codehaus.org"
-		grailsRepo "http://svn.codehaus.org/grails-plugins"
+		mavenRepo "http://repo.grails.org/grails/core"
+        mavenRepo 'http://maven.springframework.org/milestone'
 		grailsCentral()
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
 	plugins {
-		compile ":hibernate:$grailsVersion"
-		runtime ':cloud-foundry:1.0.0.M3'		
-		test ":functional-test:1.3-SNAPSHOT"
+		runtime ':cloud-foundry:1.2.3'		
+		test ":functional-test:1.3-RC1"
 	}
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         runtime 'hsqldb:hsqldb:1.8.0.10'
+        build "org.grails:grails-plugin-tomcat:$grailsVersion"
+        runtime "org.grails:grails-plugin-hibernate:$grailsVersion"
+
     }
 }
