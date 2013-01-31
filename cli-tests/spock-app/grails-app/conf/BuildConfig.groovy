46c97c16d849daeab7f637b767f199c539c0fec9
Peter Ledbrook <peter@cacoethes.co.uk>
2013-01-31 11:16:55 +0000

    CLI test for GRAILS-9763.
    The createUnitTest closure was not honouring its templatePath argument when
    searching for plugin-provided templates, such as those in the Spock plugin.
    This test checks that create-unit-test creates a Spock specification if the
    Spock plugin is installed.

---
+++ cli-tests/spock-app/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,76 @@
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.project.work.dir = "target/$grailsVersion"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+// uncomment (and adjust settings) to fork the JVM to isolate classpaths
+//grails.project.fork = [
+//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
+//]
+
+grails.project.dependency.resolution = {
+    def usingGroovy2 = usingGroovy2()
+
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+        // excludes 'ehcache'
+    }
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+    repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        mavenLocal()
+        mavenCentral()
+
+        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+        if (usingGroovy2) {
+            test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+        }
+    }
+
+    plugins {
+        runtime ":hibernate:$grailsVersion"
+        runtime ":jquery:1.8.3"
+        runtime ":resources:1.1.6"
+        
+        if (usingGroovy2) {
+            test ":spock:0.7", {
+                excludes "spock-grails-support"
+            }
+        }
+        else {
+            test ":spock:0.7"
+        }
+
+        build ":tomcat:$grailsVersion"
+
+        compile ':cache:1.0.1'
+    }
+}
+
+private boolean usingGroovy2() {
+    try {
+        getClass().classLoader.loadClass("groovy.transform.TypeChecked")
+        return true
+    }
+    catch (ClassNotFoundException ex) {
+        return false
+    }
+}
--- cli-tests/src/CreateUnitTestTestCase.groovy
+++ cli-tests/src/CreateUnitTestTestCase.groovy
@@ -15,6 +15,35 @@ class CreateUnitTestTestCase extends AbstractCliTestCase {
         assertTrue 'unit test case was not created', new File(appDir, 'test/unit/com/demo/unit/HelloWorldTests.groovy').exists()
     }
     
+    void testCreateUnitTestWithSpock() {
+        // Prepare the test application that has Spock installed.
+        def appSource = new File(cliTestsDir, "spock-app")
+        def app = new File(baseWorkDir, "createunittest-spock-app")
+
+        copyDir appSource, app
+        workDir = app
+
+        execute(["upgrade", "-non-interactive"])
+        assertEquals "upgrade failed", 0, waitForProcess()
+
+        // Make sure the plugin is installed and compiled.
+        execute([ "compile" ])
+        assertEquals "Failed to compile the app", 0, waitForProcess()
+
+        execute([ "create-unit-test" ])
+        enterInput "com.demo.unit.HelloWorld"
+        
+        assertEquals 0, waitForProcess()
+        
+        def appDir = new File(baseWorkDir, 'app1')
+        def expectedTestFile = new File(app, 'test/unit/com/demo/unit/HelloWorldSpec.groovy')
+        
+        assertTrue 'Spock unit test case was not created', expectedTestFile.exists()
+
+        def fileContent = expectedTestFile.text
+        assertTrue 'Unit test case is not a Spock test', fileContent.contains(" extends Specification")
+    }
+    
     void testSuffixStripping() {
         execute([ "create-unit-test" ])
         enterInput "com.demo.unit.DemoTests"
@@ -25,5 +54,17 @@ class CreateUnitTestTestCase extends AbstractCliTestCase {
         
         assertTrue 'unit test case was not created', new File(appDir, 'test/unit/com/demo/unit/DemoTests.groovy').exists()
         assertFalse 'test with wrong name was created', new File(appDir, 'test/unit/com/demo/unit/DemoTestsTests.groovy').exists()
+    }
+
+    private copyDir(source, destination) {
+        if (source.directory) {
+            if (!destination.exists()) {
+                assert destination.mkdir() : "making $destination"
+            }
+            source.eachFile { copyDir(it, new File(destination, it.name)) }
+        } else {
+            destination.createNewFile()
+            destination << source.newInputStream()
+        }
     }
 }
