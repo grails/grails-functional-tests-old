8c3b607d7d78d0b1ac0c226f5d19c9e7d9f426d2
Jeff Brown <jbrown@vmware.com>
2013-04-29 22:53:33 -0500

    fix compiler error

--- apps/kitchen_sink_app/src/groovy/databinding/converters/AddressValueConverter.groovy
+++ apps/kitchen_sink_app/src/groovy/databinding/converters/AddressValueConverter.groovy
@@ -4,6 +4,10 @@ import org.grails.databinding.converters.ValueConverter
 
 class AddressValueConverter implements ValueConverter {
 
+    boolean canConvert(value) {
+        value instanceof String
+    }
+
     def convert(value) {
         def pieces = value.split(':')
         new databinding.Address(city: pieces[0], state: pieces[1])
