719dbbd2404aa7e950715db67f640acd314c906e
Jeff Brown <jbrown@vmware.com>
2013-05-21 13:08:38 -0500

    GRAILS-10092 - troubleshooting test failure on hudson.grails.org

--- apps/kitchen_sink_app/test/functional/namespace/NamespacedControllerFunctionalTests.groovy
+++ apps/kitchen_sink_app/test/functional/namespace/NamespacedControllerFunctionalTests.groovy
@@ -2,16 +2,14 @@ package namespace
 
 class NamespacedControllerFunctionalTests extends functionaltestplugin.FunctionalTestCase {
 
-    void testReverseUrlMappingsForNamespacedControllers() {
+    void testNamespacedControllers() {
         get '/namespaceInspector/generateLinksToNamespacedControllers'
         assertStatus 200
 
         assertContentContains '<a href="/kitchen_sink_app/invokePrimaryController">Click to visit the controller in the primary namespace</a>'
         assertContentContains '<a href="/kitchen_sink_app/invokeSecondaryController">Click to visit the controller in the secondary namespace</a>'
         assertContentContains '<a href="/kitchen_sink_app/nonNamespacedController/index">Click to visit the controller that does not specify a namespace</a>'
-    }
 
-    void testControllerNamespaceAsVariableInUrlMapping() {
         get '/primary/namespaced'
         assertStatus 200
         assertContentContains 'Rendered by the index action in the namespace.alpha.NamespacedController controller'
@@ -27,9 +25,7 @@ class NamespacedControllerFunctionalTests extends functionaltestplugin.Functiona
         get '/secondary/namespaced/demo'
         assertStatus 200
         assertContentContains 'Rendered by the demo action in the namespace.beta.NamespacedController controller'
-    }
 
-    void testControllerNamespaceWithHardcodedNamespaceInUrlMapping() {
         get '/invokePrimaryController'
         assertStatus 200
         assertContentContains 'Rendered by the index action in the namespace.alpha.NamespacedController controller'
@@ -37,9 +33,7 @@ class NamespacedControllerFunctionalTests extends functionaltestplugin.Functiona
         get '/invokeSecondaryController'
         assertStatus 200
         assertContentContains 'Rendered by the index action in the namespace.beta.NamespacedController controller'
-    }
 
-    void testNonNamespacedControllerWithSameNameAsNamespacedControllers() {
         get '/nonNamespacedController'
         assertStatus 200
         assertContentContains 'Rendered by the index action in the namespace.gamma.NamespacedController controller'
