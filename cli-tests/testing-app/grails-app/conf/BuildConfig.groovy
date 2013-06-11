3484dbfc963203acc1e14d2f9131c6fde9f202d6
Graeme Rocher <graeme.rocher@gmail.com>
2013-06-11 13:47:15 +0200

    fix testing app to use maven dependency resolver

--- cli-tests/testing-app/grails-app/conf/BuildConfig.groovy
+++ cli-tests/testing-app/grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = "target"
 
+grails.project.dependency.resolver="maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits( "global" ) {
