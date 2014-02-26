3806a6d1d920d2470bf54c8336623e5bb5b45869
Jeff Scott Brown <jbrown@gopivotal.com>
2014-02-25 22:38:37 -0600

    use jquery plugin 1.11.0.2

--- cli-tests/spock-app/grails-app/conf/BuildConfig.groovy
+++ cli-tests/spock-app/grails-app/conf/BuildConfig.groovy
@@ -56,7 +56,7 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
         runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.10.2"
+        runtime ":jquery:1.11.0.2"
         runtime ":resources:1.2.1"
 
         test ":spock:0.7", {
