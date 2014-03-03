4c6230a2f844dab3024ea7f48d988bd123f765cb
Jeff Scott Brown <jbrown@gopivotal.com>
2014-03-02 22:17:12 -0600

    Grails 2.3.7 is using aspectj 1.7.4

--- cli-tests/src/DevWarTestCase.groovy
+++ cli-tests/src/DevWarTestCase.groovy
@@ -30,7 +30,7 @@ class DevWarTestCase extends AbstractCliTestCase {
         assertTrue output.contains("Environment set to development")
         assertTrue output.contains("grails.env = development")
         assertTrue output.contains("Compiling 3 GSP files for package [app1]")
-        
+
         // Check that the WAR file exists.
         def warFile = new File(workDir, "target/app1-0.1.war")
         assertTrue "WAR file does not exist, or it has the wrong path and/or filename.",
@@ -45,8 +45,8 @@ class DevWarTestCase extends AbstractCliTestCase {
             }
         }
 
-        assertTrue "aspectjweaver-1.7.2.jar file is missing from the WAR",
-                   warEntries.contains("WEB-INF/lib/aspectjweaver-1.7.2.jar")
+        assertTrue "aspectjweaver-1.7.4.jar file is missing from the WAR",
+                   warEntries.contains("WEB-INF/lib/aspectjweaver-1.7.4.jar")
         assertTrue "hibernate-core-3.6.10.Final.jar file is missing from the WAR",
                    warEntries.contains("WEB-INF/lib/hibernate-core-3.6.10.Final.jar")
         assertTrue "log4j-1.2.17.jar file is missing from the WAR",
--- cli-tests/src/WarTestCase.groovy
+++ cli-tests/src/WarTestCase.groovy
@@ -12,7 +12,7 @@ class WarTestCase extends AbstractCliTestCase {
         verifyHeader()
         assertTrue output.contains("Environment set to production")
         assertTrue output.contains("Compiling 3 GSP files for package [app1]")
-        
+
         // Check that the WAR file exists.
         def warFile = new File(workDir, "target/app1-0.1.war")
         assertTrue "WAR file does not exist, or it has the wrong path and/or filename.",
@@ -27,8 +27,8 @@ class WarTestCase extends AbstractCliTestCase {
             }
         }
 
-        assertTrue "aspectjweaver-1.7.2.jar file is missing from the WAR",
-                   warEntries.contains("WEB-INF/lib/aspectjweaver-1.7.2.jar")
+        assertTrue "aspectjweaver-1.7.4.jar file is missing from the WAR",
+                   warEntries.contains("WEB-INF/lib/aspectjweaver-1.7.4.jar")
         assertTrue "hibernate-core-3.6.10.Final.jar file is missing from the WAR",
                    warEntries.contains("WEB-INF/lib/hibernate-core-3.6.10.Final.jar")
         assertTrue "log4j-1.2.17.jar file is missing from the WAR",
