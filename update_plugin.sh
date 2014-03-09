ab580e9c02cd5803ee423c39550e6bedcf76071e
Lari Hotari <lhotari@gopivotal.com>
2014-03-09 15:11:12 +0200

    update hibernate4 version to 4.3.4.1

--- update_plugin.sh
+++ update_plugin.sh
@@ -6,4 +6,4 @@ if [ -z "$PLUGIN_NAME" -o -z "$NEW_VERSION" ]; then
    echo "example: $0 hibernate 3.6.10.1"
    exit 1
 fi
-perl -i -p -e "s/([\'\"]:${PLUGIN_NAME}:)[^'\"]+([\'\"])/\${1}${NEW_VERSION}\${2}/" `git ls-files '*/grails-app/conf/BuildConfig.groovy'`
+perl -i -p -e "s/([\'\"]:${PLUGIN_NAME}:)[^'\"]+([\'\"])/\${1}${NEW_VERSION}\${2}/" `git ls-files '*/grails-app/conf/BuildConfig.groovy'` build.xml
