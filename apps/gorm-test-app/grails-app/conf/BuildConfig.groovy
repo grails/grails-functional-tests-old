e94787b2f3b7e6a76eed2f8dd41ebb4b03c758d4
Lari Hotari <lhotari@gopivotal.com>
2014-07-23 13:08:05 -0400

    add remote-control plugin for testing

--- apps/gorm-test-app/grails-app/conf/BuildConfig.groovy
+++ apps/gorm-test-app/grails-app/conf/BuildConfig.groovy
@@ -7,19 +7,7 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
+grails.project.fork = false
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
@@ -66,6 +54,7 @@ grails.project.dependency.resolution = {
         test ":geb:$gebVersion"
         
         test ":rest-client-builder:2.0.3"
+        compile ":remote-control:1.5"
         // plugins for the compile step
         //compile ":scaffolding:2.0.3"
         //compile ':cache:1.1.5'
