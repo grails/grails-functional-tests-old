2a3fb81d29799d73507b142ae52568b6684e5b0a
Lari Hotari <lhotari@pivotal.io>
2014-08-09 14:08:36 +0300

    GRAILS-11607 fix test after merging PR #528 merge commit: https://github.com/grails/grails-core/commit/2b3651e1

--- cli-tests/src/CreateIntegrationTestTestCase.groovy
+++ cli-tests/src/CreateIntegrationTestTestCase.groovy
@@ -12,18 +12,18 @@ class CreateIntegrationTestTestCase extends AbstractCliTestCase {
         
         def appDir = new File(baseWorkDir, 'app1')
         
-        assertTrue 'integration test case was not created', new File(appDir, 'test/integration/com/demo/integration/HelloWorldSpec.groovy').exists()
+        assertTrue 'integration test case was not created', new File(appDir, 'test/integration/com/demo/integration/HelloWorldIntegrationSpec.groovy').exists()
     }
     
     void testSuffixStripping() {
         execute([ 'create-integration-test' ])
-        enterInput 'com.demo.integration.DemoSpec'
+        enterInput 'com.demo.integration.DemoIntegrationSpec'
         
         assertEquals 0, waitForProcess()
         
         def appDir = new File(baseWorkDir, 'app1')
         
-        assertTrue 'integration test case was not created', new File(appDir, 'test/integration/com/demo/integration/DemoSpec.groovy').exists()
-        assertFalse 'test with wrong name was created', new File(appDir, 'test/integration/com/demo/integration/DemoSpecSpec.groovy').exists()
+        assertTrue 'integration test case was not created', new File(appDir, 'test/integration/com/demo/integration/DemoIntegrationSpec.groovy').exists()
+        assertFalse 'test with wrong name was created', new File(appDir, 'test/integration/com/demo/integration/DemoIntegrationSpecIntegrationSpec.groovy').exists()
     }
 }
