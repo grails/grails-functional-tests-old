f3d519c54193b042a19cd9182aeb3fe5b02d2b38
Lari Hotari <lhotari@pivotal.io>
2014-10-11 08:48:36 +0300

    use resources plugin 1.2.13

--- apps/kitchen_sink_app/grails-app/conf/BuildConfig.groovy
+++ apps/kitchen_sink_app/grails-app/conf/BuildConfig.groovy
@@ -53,7 +53,7 @@ grails.project.dependency.resolution = {
         compile ":freemarker:0.3"
 
         compile ":jquery:1.11.1"
-        compile ":resources:1.2.13-SNAPSHOT"
+        compile ":resources:1.2.13"
 
         runtime ":database-migration:1.4.0"
 
